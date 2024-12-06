5efba7af913fa6259d36f61dbddd7d9f06961334
mendozoveh <mindozoveh@gmail.com>
2024-12-06 04:05:50 +0300

    master

--- BatleSite.cpp
+++ BatleSite.cpp
@@ -1,12 +1,14 @@
-﻿// BatleSite.cpp: определяет точку входа для приложения.
-//
-
+﻿
 #include "BatleSite.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	cout << "Hello CMake." << endl;cout << "Hello CMake." << endl;
 	return 0;
+	const clock_t begin_time = clock();
+	// do something
+	std::cout << float(clock() - begin_time) / CLOCKS_PER_SEC;
+	
 }
